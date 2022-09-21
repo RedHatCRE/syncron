32ef8abfbcd633af37317ff2729a2849b41cf03d
Jorge San Emeterio <jsanemet@redhat.com>
2022-09-21 14:57:09 +0200

    Documenting packages too

--- pkg/cli/args.go
+++ pkg/cli/args.go
@@ -1,13 +1,13 @@
 package cli
 
-// Enumerates all keywords used as arguments on the CLI.
+// Keywords used as arguments on the CLI.
 const (
 	INSIGHTS_A   string = "insights"
 	SOSREPORTS_A string = "sosreports"
 	ALL_A        string = "all"
 )
 
-// Enumerates all keywords used as flags on the CLI.
+// Keywords used as flags on the CLI.
 const (
 	DEBUG_F string = "debug"
 
@@ -16,7 +16,7 @@ const (
 	YEARS_F  string = "years"
 )
 
-// Enumerates all keywords used as shortcuts on the CLI.
+// Keywords used as shortcuts on the CLI.
 const (
 	DEBUG_P string = "d"
 )
---
+++ pkg/log/doc.go
@@ -0,0 +1,3 @@
+// Package log provides utilities to get Syncron's log framework (Logrus)
+// to behave as expected by the app.
+package log
