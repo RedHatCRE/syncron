d1bc504ed473cae9eeb042c44c7a93e56f77e90e
Jorge San Emeterio <jsanemet@redhat.com>
2022-09-21 15:19:20 +0200

    Creating skeleton for Configure tests

---
+++ pkg/log/config_test.go
@@ -0,0 +1,20 @@
+package log_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type ConfigureTestSuite struct {
+	suite.Suite
+}
+
+func (suite *ConfigureTestSuite) TestLevelOnNoDebug() {
+	assert.True(suite.T(), false)
+}
+
+func TestConfigureTestSuite(t *testing.T) {
+	suite.Run(t, new(ConfigureTestSuite))
+}
