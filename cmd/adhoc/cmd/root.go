b31dd6432e925c44962c2fc33f431ba68d01fb13
Ignacio Horcada Bernal <ibernal@redhat.com>
2022-10-05 12:34:23 +0200

    Changed function location

--- cmd/adhoc/cmd/root.go
+++ cmd/adhoc/cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"os"
 
 	"github.com/rhcre/syncron/pkg/cli"
 	"github.com/rhcre/syncron/pkg/log"
@@ -28,41 +27,31 @@ func init() {
 	)
 }
 
-func readFile() {
-	// Setting up file formatting
-	viper.SetConfigFile("syncron.yaml")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-	// Reading from file
-	err := viper.ReadInConfig()
-	if err != nil {
-		logrus.Error("Config file not found.\n")
-		logrus.Info("Make sure syncron.yaml is in the correct folder. \n")
-		logrus.Info("Syncron looks for the config file on: \n")
-		logrus.Info("	- Root of the repository \n")
-		os.Exit(1)
-	} else {
-		logrus.Info("Your configuration file was read succesfully!")
-	}
-	private_key := viper.Get("credentials.aws_secret_access_key")
-	public_key := viper.Get("credentials.aws_access_key_id")
-	bucket_name := viper.Get("buckets.sosreports")
-	logrus.Info("aws_secret_access_key: ", private_key)
-	logrus.Info("aws_public_access_key: ", public_key)
-	logrus.Info("Reading from bucket: ", bucket_name)
-}
-
 func onPersistentPreRun(cmd *cobra.Command, args []string) error {
 	// Define actions taken to get Syncron ready before commands are executed
 	setup := []func() error{
 		// Set up logging
 		func() error {
 			parser := cli.NewParserForCobra(cmd, args)
-
 			return log.Configure(parser)
 		},
+		func() error {
+			// Setting up file formatting
+			viper.SetConfigFile("syncron.yaml")
+			viper.SetConfigType("yaml")
+			viper.AddConfigPath(".")
+			// Reading from file
+			err := viper.ReadInConfig()
+			if err != nil {
+				return err
+			} else {
+				logrus.Info("Your configuration file was read succesfully!")
+				bucket_name := viper.Get("buckets.sosreports")
+				logrus.Info("Reading from bucket: ", bucket_name)
+			}
+			return err
+		},
 	}
-	readFile()
 	for _, step := range setup {
 		err := step()
 
@@ -70,6 +59,5 @@ func onPersistentPreRun(cmd *cobra.Command, args []string) error {
 			return err
 		}
 	}
-
 	return nil
 }
