3c0ad8b77f5d632cdd01bcc1d195571a0cd3d1b1
IgnacioPro <43138083+IgnacioPro@users.noreply.github.com>
2022-11-04 12:09:35 +0100

    Implement file downloads  (#18)
    Implementation of download keys from s3 bucket

--- pkg/bucketaws/setup.go
+++ pkg/bucketaws/setup.go
@@ -17,65 +17,119 @@ package s3setup
 
 import (
 	"fmt"
-	"os"
+	"strings"
+	"time"
+
+	files "github.com/redhatcre/syncron/utils"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-func ConfigRead() error {
-	// Setting up file formatting
-	// Pulling from Viper
-	viper.SetConfigFile("config/syncron.yaml")
+// Builds dates from given date to current date,
+// incrementing one day at a time.
+func ProcessDate(fromDate time.Time) []string {
+
+	start := fromDate
+	end := time.Now()
+	var dates []string
+	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
+		year, month, day := d.Date()
+		dates = append(
+			dates,
+			fmt.Sprintf(
+				"created_year=%d/created_month=%d/created_day=%d",
+				year, month, day))
+	}
+	return dates
+}
+
+// Setting up file formatting
+// Using Viper
+// Reading from file syncron.yaml
+func ConfigRead() {
+
+	viper.AddConfigPath("./config")
+	viper.SetConfigFile("syncron.yaml")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
+	viper.SetDefault("download_dir", "/tmp/syncron/")
+
 	// Reading from file
 	err := viper.ReadInConfig()
 	if err != nil {
-		return err
-	} else {
-		logrus.Info("Your configuration file was read succesfully")
-		logrus.Info("Reading from bucket: ", viper.Get("bucket"))
+		logrus.Fatal(err)
 	}
-	return nil
+	logrus.Info("Your configuration file was read succesfully")
+	logrus.Info("Reading from bucket: ", viper.Get("bucket"))
 }
 
+// Initialize a session with AWS SDK
+// It will read from the file located at ~/.aws/credentials and syncron/syncron.yaml
 func SetupSession() *session.Session {
-	// Initialize a session with AWS SDK
-	// It will read from the file located at ~/.aws/credentials and syncron/config/syncron.yaml
+
 	sess, err := session.NewSession(&aws.Config{
 		Region:   aws.String(viper.GetString("s3.region")),
 		Endpoint: aws.String(viper.GetString("s3.endpoint")),
 	},
 	)
 	if err != nil {
-		fmt.Println("There was an error setting up your aws session")
-		os.Exit(1)
-	} else {
-		logrus.Info("Your AWS session was set up correctly")
+		logrus.Fatal("There was an error setting up your aws session", err)
 	}
+	logrus.Info("Your AWS session was set up correctly")
 	return sess
 }
 
-func AccessBucket(sess *session.Session) error {
+// This function initiates the service for downloading files in s3
+func AccessBucket(sess *session.Session) (*s3.S3, *s3manager.Downloader) {
+
 	logrus.Info("Accessing bucket...")
 	svc := s3.New(sess)
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(viper.GetString("bucket"))})
-	if err != nil {
-		fmt.Println("Unable to list items in bucket", resp)
-		return err
-	} else {
-		logrus.Info("Success getting into the bucket!")
-	}
-	return nil
+	dwn := s3manager.NewDownloader(sess)
+
+	return svc, dwn
 }
 
-func DownloadFromBucket() error {
+// This function takes care of listing the keys in the bucket, filtering
+// through those that are needed.
+func DownloadFromBucket(svc *s3.S3, dwn *s3manager.Downloader, dates []string, bprefix string) error {
 
-	// Future PR implementation
+	var continuationToken *string
 
+	for {
+		resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+			Bucket:            aws.String(viper.GetString("bucket")),
+			Prefix:            aws.String(files.AppendPrefix(bprefix)),
+			ContinuationToken: continuationToken,
+		})
+		if err != nil {
+			logrus.Fatal("There was an error listing the objects in bucket.", err)
+		}
+		for _, item := range resp.Contents {
+			for _, x := range dates {
+				if strings.Contains(*item.Key, x) {
+					fooFile, fileName := files.FilePathSetup(item.Key, dwn)
+					logrus.Info("Downloading ", fileName)
+					_, err := dwn.Download(
+						fooFile,
+						&s3.GetObjectInput{
+							Bucket: aws.String(viper.GetString("bucket")),
+							Key:    aws.String(*item.Key),
+						})
+					if err != nil {
+						fmt.Println("There was an error fetching key info.", err)
+						return err
+					}
+				}
+			}
+		}
+		if !aws.BoolValue(resp.IsTruncated) {
+			break
+		}
+		continuationToken = resp.NextContinuationToken
+	}
 	return nil
 }
