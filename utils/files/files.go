af9c16e894dabe02d72d1868c54393ad4c900ec8
Adrian Fusco Arnejo <adrianfuscoarnejo@gmail.com>
2022-11-22 17:55:21 +0100

    - Remove dependency of s3manager package in the files package - Added FileExists method to check if a file is already downloaded - Deleted the GetConfigPath function since os.UserConfigDir append the .config folder automatically in UNIX based distributions

--- utils/files/files.go
+++ utils/files/files.go
@@ -20,29 +20,34 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-// This function takes a key from s3 and prepares the system for
-// download by recursively creating the path needed.
-func FilePathSetup(key *string, dwn *s3manager.Downloader) (*os.File, string) {
-
-	download_dir := viper.GetString("download_dir")
-	err := os.MkdirAll(download_dir+filepath.Dir(*key), 0700)
-	if err != nil {
-		logrus.Fatal("An error ocurred creating paths", err)
-	}
-	fileName := filepath.Base(*key)
-
-	fileHandler, err := os.Create(filepath.Clean(download_dir + filepath.Dir(*key) + "/" + fileName))
-
-	if err != nil {
-		fmt.Println(err)
+// Creates a folder/file given the path in a recursive way
+// If the path finish with '/' it will create a file
+// Otherwise it will create a folder
+func FilePathSetup(absoluteFilePath string) *os.File {
+	if err := os.MkdirAll(filepath.Dir(absoluteFilePath), 0750); err != nil {
+		if err != nil {
+			logrus.Fatal(err)
+		}
 	}
 
-	return fileHandler, fileName
+	fileHandler, err := os.Create(filepath.Clean(absoluteFilePath))
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	return fileHandler
+}
+
+func FileExists(filePath string) bool {
+	if _, err := os.Stat(filePath); err == nil {
+		return true
+	}
+	return false
 }
 
 // This function appends the prefix to components
@@ -52,12 +57,4 @@ func AppendPrefix(bPrefix string) string {
 	prefix := viper.GetString("prefix")
 	fullPrefix := fmt.Sprint(prefix + bPrefix)
 	return fullPrefix
-}
-
-func GetConfigPath() string {
-	dirname, err := os.UserHomeDir()
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	return dirname + "/.config/"
 }
